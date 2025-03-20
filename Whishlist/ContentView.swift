d8b8015b02325e45588be07bf50e6de949da6aa4
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-20 16:13:57 +0100

    Add BottomToolbar to count the number of whishes

--- Whishlist/ContentView.swift
+++ Whishlist/ContentView.swift
@@ -40,6 +40,12 @@ struct ContentView: View {
                             .imageScale(.large)
                     }
                 }
+
+                if wishes.isEmpty != true {
+                    ToolbarItem(placement: .bottomBar) {
+                        Text("\(wishes.count) wish\(wishes.count > 1 ? "es" : "")")
+                    }
+                }
             }
             .alert("Create a new wish", isPresented: $isAlertShowing) {
                 TextField("Enter a wish", text: $title)

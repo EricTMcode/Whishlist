bb7c1a8ed3921b3ecdfb5516f631e70a69e2394a
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-20 16:08:16 +0100

    Add button to save wish

--- Whishlist/ContentView.swift
+++ Whishlist/ContentView.swift
@@ -12,27 +12,52 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var wishes: [Wish]
 
+    @State private var isAlertShowing = false
+    @State private var title = ""
+
     var body: some View {
         NavigationStack {
             List {
                 ForEach(wishes) { wish in
                     Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+
                 }
             }
             .navigationTitle("Wishlist")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isAlertShowing.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                            .imageScale(.large)
+                    }
+                }
+            }
+            .alert("Create a new wish", isPresented: $isAlertShowing) {
+                TextField("Enter a wish", text: $title)
+
+                Button("Save") {
+                    modelContext.insert(Wish(title: title))
+                    try? modelContext.save()
+                    title = ""
+                }
+            }
             .overlay {
                 if wishes.isEmpty {
-                    ContentUnavailableView("My Whishlist", systemImage: "heart.circle", description: Text("No wishes yet. Add one to get started."))
+                    ContentUnavailableView("My Whishlist",
+                        systemImage: "heart.circle",
+                        description: Text("No wishes yet. Add one to get started.")
+                    )
                 }
             }
         }
     }
 }
 
-#Preview("Empty List") {
-    ContentView()
-        .modelContainer(for: Wish.self, inMemory: true)
-}
+
 
 #Preview("List with Sample Data") {
     let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
@@ -45,4 +70,9 @@ struct ContentView: View {
 
     return ContentView()
         .modelContainer(container)
+}
+
+#Preview("Empty List") {
+    ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }

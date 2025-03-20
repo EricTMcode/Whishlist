958bc5b402fe381bb737687b1dca11e0f81f099d
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-20 15:30:32 +0100

    Initial Commit

---
+++ Whishlist/WhishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhishlistApp.swift
+//  Whishlist
+//
+//  Created by Eric on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
